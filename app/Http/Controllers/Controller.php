963ff0f09d713e943e80f297ed3a58e8168b5aa7
abela-a <work.abelardhana@gmail.com>
2025-04-26 23:47:36 +0800

    Add L5 Swagger integration and update environment configuration

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use OpenApi\Attributes as OA;
+
+#[
+    OA\Info(
+        title: 'Sales & Order Management API',
+        version: '1.0.0',
+        description: 'Technical test for backend developer position at PT. Dibuiltadi Teknologi Kreatif',
+    ),
+    OA\Server(url: 'http://localhost:8000/api/v1', description: 'Local V1'),
+    OA\Contact(
+        name: 'Abel Ardhana Simanungkalit',
+        url: 'https://github.com/abela-a',
+        email: 'work.abelardhana@gmail.com',
+    )
+]
 abstract class Controller
 {
     //
