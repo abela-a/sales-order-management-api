a26c93b9e75c8a48c51fad47c3836c1833ec529c
abela-a <work.abelardhana@gmail.com>
2025-04-26 12:34:24 +0800

    Add Customer model with mass assignable attributes and orders relationship

---
+++ app/Models/Customer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'address',
+        'phone',
+    ];
+
+    /**
+     * Define a one-to-many relationship with the Order model.
+     *
+     * This method establishes that a Customer can have multiple orders.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+}
